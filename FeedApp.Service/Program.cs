0a49fa75ecd9effdc8d9ffe126bae542ec840e21
andersbuanes <anderstbuanes@hotmail.com>
2022-11-11 19:03:57 +0100

    add mapper to messaging service

--- FeedApp.Service/Program.cs
+++ FeedApp.Service/Program.cs
@@ -2,15 +2,23 @@ using FeedApp.Service;
 using FeedApp.Messaging.Sender;
 using FeedApp.Messaging.Options;
 using FeedApp.Api.Proxies.Data;
+using FeedApp.Api.Mappers;
+using FeedApp.Api.Utils;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Services
 builder.Services.AddScoped<PublishService>();
 builder.Services.AddSingleton<PeriodicHostedService>();
 builder.Services.AddHostedService(
     provider => provider.GetRequiredService<PeriodicHostedService>());
+
+// Mappers
+builder.Services.AddScoped<IWebMapper, WebMapper>();
 
 // Messaging
 builder.Services.Configure<RabbitMqConfiguration>(builder.Configuration.GetSection("RabbitMq"));
@@ -21,6 +29,9 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSql"));
     options.UseLazyLoadingProxies();
 });
+
+// Utils
+builder.Services.AddScoped<IPollUtils, PollUtils>();
 
 var app = builder.Build();
 
